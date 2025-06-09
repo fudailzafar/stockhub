6a5f59579009a84923c29c4c53958f4a60fbf2a9
Fudail Mohammed Zafar <142711828+fudailzafar@users.noreply.github.com>
2025-06-09 18:22:23 +0530

    Created Login API

--- server/server.js
+++ server/server.js
@@ -2,19 +2,19 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import adminRouter from "./routes/adminRoutes.js";
 
 const app = express();
 
-await connectDB
+await connectDB();
 
 // Middlewares
-
 app.use(cors());
 app.use(express.json());
 
 // Routes
-
 app.get("/", (req, res) => res.send("API is working"));
+app.use("/api/admin", adminRouter);
 
 const PORT = process.env.PORT || 3000;
 
