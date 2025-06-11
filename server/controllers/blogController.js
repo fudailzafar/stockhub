ce99f83f1de6ed23985af49143a86fa843360bcc
Fudail Mohammed Zafar <142711828+fudailzafar@users.noreply.github.com>
2025-06-11 12:36:27 +0530

    Blog image upload on ImageKit

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,3 +1,7 @@
+import fs from "fs";
+import imagekit from "../configs/imageKit.js";
+import Blog from "../models/Blog.js";
+
 export const addBlog = async (req, res) => {
   try {
     const { title, subTitle, description, category, isPublished } = JSON.parse(
@@ -9,5 +13,39 @@ export const addBlog = async (req, res) => {
     if (!title || !description || !category || !imageFile) {
       return res.json({ success: false, message: "Missing required fields" });
     }
-  } catch (error) {}
+
+    const fileBuffer = fs.readFileSync(imageFile.path);
+
+    // Upload Image to ImageKit
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/blogs",
+    });
+
+    // Optimised image transformation
+    const optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { quality: "auto" }, // Auto compression
+        { format: "webp" }, // Webp format
+        { width: "1280" }, // Width Resizing
+      ],
+    });
+
+    const image = optimizedImageUrl;
+
+    await Blog.create({
+      title,
+      subTitle,
+      description,
+      category,
+      image,
+      isPublished,
+    });
+
+    res.json({ success: true, message: "Blog added successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
 };
